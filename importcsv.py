50df1ea0878ddbb11913355d239a4375966db8d4
lisanespe <lisandroespejo@gmail.com>
2023-07-04 13:39:28 -0300

    inicial commit

---
+++ importcsv.py
@@ -0,0 +1,42 @@
+import csv
+
+
+def create_statement(file_path):
+    '''Takes a file with Integration Services Metada and transform in a SQL Create Statemet '''
+    with open(file_path, 'r', encoding="utf8") as file:
+        reader = csv.reader(file, delimiter='\t')
+        next(reader)  # Skip header row
+
+        create_statement = "CREATE TABLE table_name (\n"
+
+        for row in reader:
+            column_name = row[0].strip('"')
+            data_type = row[1].strip('"')
+            precision = int(row[2].strip('"'))
+            scale = int(row[3].strip('"'))
+            length = int(row[4].strip('"'))
+
+            sql_data_type = ''
+            if data_type == 'DT_WSTR':
+                sql_data_type = f'NVARCHAR({length})'
+            elif data_type == 'DT_I2':
+                sql_data_type = 'SMALLINT'
+            elif data_type == 'DT_NUMERIC':
+                sql_data_type = f'DECIMAL({precision}, {scale})'
+            elif data_type == 'DT_DBTIMESTAMP':
+                sql_data_type = 'DATETIME'
+            elif data_type == 'DT_I4':
+                sql_data_type = 'INT'
+
+            create_statement += f'    {column_name:<25} {sql_data_type:<25} NULL,\n'
+            
+        create_statement = create_statement.rstrip(',\n')  # Remove the last comma
+        create_statement += "\n);"
+
+        return create_statement
+
+# Example usage
+FilePath = 'sap.txt'
+create_statement = create_statement(FilePath)
+with open("create.txt", "wt",encoding="utf8") as f:
+    print(create_statement, file = f)
